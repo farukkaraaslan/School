63690abd6f883ebe245b5f55d6d7a5856e87f568
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-19 15:09:08 +0300

    enttiy repository interface oluşturuşdu.

---
+++ Backend/Entity/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.DataAccess;
+
+public interface IEntityRepository<T>
+	where T : BaseEntity, new()
+{
+	T Get(Expression<Func<T, bool>> filter = null);
+	List<T> GetList(Expression<Func<T,bool>> filter = null);
+	Guid Add(T entity);
+	Guid Update(T entity);
+	Guid Delete(T entity);	
+}
+
