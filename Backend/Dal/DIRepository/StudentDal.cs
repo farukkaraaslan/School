007fdb6a2e0e14d293a29b5d1bd856f7937e0f2b
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-20 09:43:52 +0300

    dal concreta oluşturuldu

---
+++ Backend/Dal/DIRepository/StudentDal.cs
@@ -0,0 +1,15 @@
+﻿using Dal.Interface;
+using Entity.Context;
+using Entity.DataAccess;
+using Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.DIRepository;
+
+public class StudentDal : EntityRepositoryBase<Student, MyDbContext>, IStudentDal
+{
+}
