3a3be97a178205828bbbcfd8dc42b05f087011e7
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-20 15:55:58 +0300

    user credentials nesne olara tanımlandı

--- Backend/WebAPI/Security/BasicAutheticatonHandler.cs
+++ Backend/WebAPI/Security/BasicAutheticatonHandler.cs
@@ -4,17 +4,22 @@ using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
+using WebAPI.Service;
 
 namespace WebAPI.Security;
 
 public class BasicAutheticatonHandler : AuthenticationHandler<BasicAuthenticationOption>
 {
+    private readonly UserService _userService;
     public BasicAutheticatonHandler(
         IOptionsMonitor<BasicAuthenticationOption> options,
         ILoggerFactory loggerFactory,
         UrlEncoder urlEncoder,
         ISystemClock systemClock) : base(options, loggerFactory, urlEncoder, systemClock)
-    { }
+    { 
+    
+        _userService = new UserService();
+    }
 
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
@@ -29,14 +34,18 @@ public class BasicAutheticatonHandler : AuthenticationHandler<BasicAuthenticatio
         string userName = userCredential.Split(':')[0];
         string password = userCredential.Split(':')[1];
 
-        if ((userName != "Faruk") || (password != "1234"))
+        User user = _userService.ValidateUser(userName, password);
+        if (user == null)
         {
             return AuthenticateResult.Fail("Wrong username and password");
         }
 
         Claim[] claims = new Claim[]
         {
-            new Claim(ClaimTypes.Name, userName),
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.MobilePhone, user.Phone),
+            new Claim(ClaimTypes.Role, user.Role),
         };
 
         ClaimsIdentity identity = new ClaimsIdentity(claims, Scheme.Name);
---
+++ Backend/WebAPI/Service/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAPI.Service;
+
+public class UserService
+{
+    private readonly List<User> users;
+
+    public UserService()
+    {
+
+        users = new()
+        {
+            new(){Id=0,Name="Tamer",Password="Tt1234!.",Email="greenpeace@hotmail.com",Phone="5055929385",Role="Admin"},
+            new(){Id=1,Name="Ahmet",Password="Aa1234!.",Email="ahmet@hotmail.com",Phone="5342563638",Role="Editor"},
+            new(){Id=2,Name="Ebru",Password="Ee1234!.",Email="ebru@hotmail.com",Phone="5425263857",Role="Author"},
+            new(){Id=3,Name="Esin",Password="Es1234!.",Email="esin@hotmail.com",Phone="5425265874",Role="Author"},
+            new(){Id=4,Name="Neslihan",Password="Nn1234!.",Email="neslihan@hotmail.com",Phone="5536341528",Role="Author"}
+        };
+    }
+
+    public User ValidateUser(string username, string password)
+    {
+        return users.FirstOrDefault(u => u.Name == username && u.Password == password);
+    }
+}
+
